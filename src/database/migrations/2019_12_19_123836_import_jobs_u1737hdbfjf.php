b475c4234b426c2b4cd8de1d7ff541fe839a1e07
Vijay S <vijay@uitoux.in>
2019-12-20 12:43:04 +0530

    changed while running error on 52 so jide this

--- src/database/migrations/2019_12_19_123836_import_jobs_u1737hdbfjf.php
+++ src/database/migrations/2019_12_19_123836_import_jobs_u1737hdbfjf.php
@@ -14,7 +14,7 @@ class ImportJobsU1737hdbfjf extends Migration {
 		Schema::table('import_jobs', function (Blueprint $table) {
 			$table->unsignedInteger('entity_id')->nullable()->change();
 			$table->text('error_details')->nullable()->change();
-			$table->dropForeign('import_jobs_type_id_foreign');
+			// $table->dropForeign('import_jobs_type_id_foreign');
 			$table->foreign('type_id')->references('id')->on('import_types')->onDelete('CASCADE')->onUpdate('cascade');
 
 		});
