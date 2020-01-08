a52ce6eb381cd1b5e027ec8cfb4b312e148d2f3c
Abdul <abdul@uitoux.in>
2020-01-08 18:51:38 +0530

    Update ImportCronJobPkgServiceProvider.php

--- src/ImportCronJobPkgServiceProvider.php
+++ src/ImportCronJobPkgServiceProvider.php
@@ -14,7 +14,7 @@ class ImportCronJobPkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-		$this->loadViewsFrom(__DIR__ . '/views', 'import-cron-job-pkg');
+		$this->loadViewsFrom(__DIR__ . '/views', 'import-cron-job');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
 		]);
