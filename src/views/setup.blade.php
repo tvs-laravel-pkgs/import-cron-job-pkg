d28edb55685d3259be11cf49e749509f3ff0ce76
kaliga <kaliga@uitoux.in>
2020-01-09 13:11:46 +0530

    u

--- src/views/setup.blade.php
+++ src/views/setup.blade.php
@@ -7,8 +7,8 @@
 <script type="text/javascript">
     var import_cron_job_list_template_url = "{{URL::asset($import_cron_job_pkg_path.'public/angular/import-cron-job-pkg/pages/import-cron-job/list.html')}}";
     var import_cron_job_from_template_url = "{{URL::asset($import_cron_job_pkg_path.'public/angular/import-cron-job-pkg/pages/import-cron-job/form.html')}}";
-    var import_cron_job_from_data_check = "{{url('import-cron-job-pkg/import-jobs/get-from-data/')}}";
+    var import_cron_job_from_data_url = "{{url('import-cron-job-pkg/import-jobs/get-from-data/')}}";
     var import_cron_job_delete = "{{url('import-cron-job-pkg/import-job/delete/')}}";
-    var import_cron_job_template_file_url = "{{URL::asset('/public/templates/')}}";
+    var import_cron_job_template_base_path = "{{URL::asset('/public/templates/')}}";
 </script>
 <script type="text/javascript" src="{{URL::asset($import_cron_job_pkg_path.'public/angular/import-cron-job-pkg/pages/import-cron-job/controller.js')}}"></script>
