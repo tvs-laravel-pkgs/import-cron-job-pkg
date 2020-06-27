d7075d8e5014f65edecb6f5f5bc589fcd51fdc76
Abdul <abdul@uitouxsolutions.com>
2020-06-27 13:12:12 +0530

    u

--- src/services/BulkImportExportService.php
+++ src/services/BulkImportExportService.php
@@ -40,6 +40,10 @@ abstract class BulkImportExportService {
 			foreach ($sheets as $key => $sheet_detail) {
 				$sheet_name = $sheet_detail['sheet_name'];
 				dump($sheet_name . ' STARTED');
+				if (empty($sheet_detail['import_type'])) {
+					dump('Import Type is empty');
+					continue;
+				}
 				$import_type = ImportType::where('name', $sheet_detail['import_type'])->first();
 				if (!$import_type) {
 					dump('Import Type not found : ' . $sheet_detail['import_type']);
@@ -50,8 +54,13 @@ abstract class BulkImportExportService {
 					$reader->skipRows($sheet_detail['skip']);
 					$reader->takeRows($sheet_detail['row_limit']);
 					$records = $reader->get();
+					// dump('Executing ' . $import_type->action);
 					$errors = call_user_func($import_type->action, $records, $company, $specific_company, $tc);
-					$all_error_records = array_merge($all_error_records, $errors);
+					if (!is_array($errors)) {
+						dump($sheet_name . ' Action Function not found');
+					} else {
+						$all_error_records = array_merge($all_error_records, $errors);
+					}
 				});
 				dump($sheet_name . ' COMPLETED');
 			}
@@ -66,7 +75,8 @@ abstract class BulkImportExportService {
 			]);
 
 		} catch (\Throwable $e) {
-			$job->status_id = 7203; //Error
+			//check before commit
+			// $job->status_id = 7203; //Error
 			$job->error_details = 'Error:' . $e->getMessage() . '. Line:' . $e->getLine() . '. File:' . $e->getFile(); //Error
 			$job->save();
 			dump($job->error_details);
